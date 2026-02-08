45dd8eb80087856f1c065bc9f2dc98ec63044efa
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-02-08 12:50:47 +0530

    msg added

--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -30,3 +30,22 @@ const requiredEnv = [
     TWILIO_AUTH_TOKEN : process.env.TWILIO_AUTH_TOKEN,
     TWILIO_PHONE_NUMBER : process.env.TWILIO_PHONE_NUMBER,
   }
+
+
+  /*
+  
+  PORT=3000
+GOOGLE_GEMINI_KEY= add your google gemini key here
+JWT_SECRET= add your jwt secret here
+MONGO_URI= add your mongo uri here
+
+IMAGEKIT_PUBLIC_KEY= add your imagekit public key here
+IMAGEKIT_PRIVATE_KEY= add your imagekit private key here
+IMAGEKIT_URL= add your imagekit url here
+
+TWILIO_ACCOUNT_SID= add your twilio account sid here
+TWILIO_AUTH_TOKEN= add your twilio auth token here
+TWILIO_PHONE_NUMBER= add your twilio phone number here
+
+  
+  */
