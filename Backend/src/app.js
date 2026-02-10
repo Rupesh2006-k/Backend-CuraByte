f6f21a50a54b6d7d0fd28559bf163052dd3109fd
iamsaba786 <snaz7571@gmail.com>
2026-02-11 02:42:48 +0530

    feat: additional feature register user updates

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
-let aiRoutes = require('./routes/ai.routes')
-let userRoutes = require('./routes/user.routes');
+const cors = require("cors");
+let aiRoutes = require("./routes/ai.routes");
+let userRoutes = require("./routes/user.routes");
 const connectDB = require("./db/connectDB");
-const app = express()
+const app = express();
 
 // ================== MIDDLEWARES ==================
-connectDB()
+connectDB();
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -16,8 +24,8 @@ app.use(morgan("dev"));
 
 // ================== ROUTES ==================
 
-app.use('/api/ai' , aiRoutes)
-app.use('/api/auth' ,userRoutes)
+app.use("/api/ai", aiRoutes);
+app.use("/api/auth", userRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({
